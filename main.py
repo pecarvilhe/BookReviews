effdd0e975a948780930c274d770fe06970ff8a2
Pedro Carvilhe <pecarvilhe1506@gmail.com>
2026-04-14 21:58:45 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="wide")
+
+df_review = pd.read_csv("datasets/customer reviews.csv")
+df_top100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
+
+df_review
