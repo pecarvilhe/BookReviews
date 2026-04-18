7b3d5fa20574a563dfb0ea165599042e4a760ee6
Pedro Carvilhe <pecarvilhe1506@gmail.com>
2026-04-18 18:53:18 -0300

    add plotly graphics

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 st.set_page_config(layout="wide")
 
@@ -12,3 +13,10 @@ price_min = df_top100_books["book price"].min()
 max_price = st.sidebar.slider("Price Range", price_min, price_max, price_max)
 df_books = df_top100_books[df_top100_books["book price"] <= max_price]
 df_books
+
+fig_year = px.bar(df_top100_books["year of publication"].value_counts());
+fig_price = px.histogram(df_top100_books["book price"])
+
+col1, col2 = st.columns(2)
+col1.plotly_chart(fig_year)
+col2.plotly_chart(fig_price)
